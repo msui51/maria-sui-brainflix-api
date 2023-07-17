a1c856ccd99125c68f8f1d328f0b5c3462e0e89c
Maria Sui <maria.sui51@gmail.com>
2023-07-16 21:21:35 -0400

    finished routes

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const cors = require("cors");
 const videoRoutes = require("./routes/videos");
 
 app.use(cors());
+
+//middleware to grab req.body
+
 app.use(express.json());
+
+//middleware to read static files
+
 app.use(express.static("public"));
+
+//middleware for routes that start with /videos
+
 app.use("/videos", videoRoutes);
 
-
+//specifying the port the server is listening to
 app.listen(5000,()=>{
     console.log("server started on port 5000")
 })
--- routes/videos.js
+++ routes/videos.js
@@ -2,16 +2,18 @@ const router = require("express").Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 
+//getting all videos
 
 router.get('/', (req,res)=>{
     fs.readFile("./data/video-details.json", "utf-8", (err, data)=>{
         if(err){
             return res.send("error")
         }
-        console.log(JSON.parse(data))
         res.json(JSON.parse(data));
     });
 });
+
+//getting individual videos
 
 router.get('/:id', (req,res)=>{
     fs.readFile("./data/video-details.json", "utf-8", (err, data)=>{
@@ -24,17 +26,8 @@ router.get('/:id', (req,res)=>{
     });
 });
 
-router.get("/:id/comments", (req,res)=>{
-    fs.readFile("./data/video-details.json", "utf-8",(err,data)=>{
-        if(err){
-            console.log("err");
-        }
-        const videos=JSON.parse(data);
-        const singleVideo=videos.find((video)=>video.id==req.params.id);
-        const comments = singleVideo.comments;
-        res.json(comments);
-    })
-})
+
+//posting a new video
 
 router.post("/", (req, res)=>{
     fs.readFile("./data/video-details.json", "utf-8", (err,data)=>{
@@ -46,7 +39,7 @@ router.post("/", (req, res)=>{
             id: uuidv4(),
             title: req.body.title,
             channel: "Snowball",
-            image: "/image9.jpeg",
+            image: "/images/image9.jpeg",
             description: req.body.description,
             views: 0,
             likes: 0,
@@ -65,17 +58,16 @@ router.post("/", (req, res)=>{
     })
 })
 
+//posting a new comment
 
 router.post("/:id/comments", (req,res)=>{
     fs.readFile("./data/video-details.json", "utf-8", (err,data)=>{
         if(err){
             return res.send("error");
         }
-        
         const videos=JSON.parse(data);
         const singleVideo=videos.find((video)=>video.id == req.params.id);
-        const commentList=singleVideo.comments;
-       
+        const commentList=singleVideo.comments; 
         const newComment={
             id: uuidv4(),
             comment: req.body.comment,
@@ -88,11 +80,41 @@ router.post("/:id/comments", (req,res)=>{
             if(err){
                 return res.send("err");
             }
-            res.send("game saved")
+            res.send("comment saved")
         });
     });
 });
 
+//getting a single video's comments
+
+router.get("/:id/comments", (req,res)=>{
+    fs.readFile("./data/video-details.json", "utf-8",(err,data)=>{
+        if(err){
+            return res.send("err");
+        }
+        const videos=JSON.parse(data);
+        const singleVideo=videos.find((video)=>video.id==req.params.id);
+        const comments = singleVideo.comments;
+        res.json(comments);
+    });
+})
+
+//deleing a comment from a single video
+
+router.delete("/:id/comments/:commentsId", (req, res)=>{
+    const {id, commentsId} =req.params;
+    const data= fs.readFileSync("./data/video-details.json")
+    const videos=JSON.parse(data);
+    const singleVideo=videos.find((video)=>video.id === id);
+    const commentsList = singleVideo.comments;
+    const commentToDelete=commentsList.find((comment)=>comment.id === commentsId);
+    const index=commentsList.indexOf(commentToDelete);
+        
+    commentsList.splice(index,1);
+
+    fs.writeFileSync("./data/video-details.json", JSON.stringify(videos));
+    res.send("comment deleted");
+})
 
 
 module.exports = router;
